055111e484280b8707a18b4641006ee06057b07f
fredowisk <zcrazyhero@gmail.com>
2023-09-20 20:24:28 -0300

    app files

---
+++ Models/Iphone.cs
@@ -0,0 +1,16 @@
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeDoApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeDoApp}' no iPhone.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando o Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "9 1234-5678", modelo: "C12", imei: "123456789101112", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n--------\n");
+
+Console.WriteLine("Testando o Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "9 8765-4321", modelo: "15", imei: "121110987654321", memoria: 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
